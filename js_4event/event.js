988bbd8504cfdaefe95a64b48b30df4c41f1c969
stgulaa <51143932+stgulaa@users.noreply.github.com>
2021-07-16 20:23:57 +0900

    event2
    여러가지 이벤트 생성해보기

--- js_4event/event.js
+++ js_4event/event.js
@@ -2,10 +2,21 @@ const title = document.querySelector(".hello h1");
 console.dir(title);
 
 //글자 색 변경
-title.style.color="blue";
+//title.style.color="blue";
 
 function handTitleClick(){
     console.log("title was cliked");
 }
 
+function handleMouseEnter(){
+    //console.log("mouse is here");
+    title.innerText ="mouse is here";
+}
+function handleMouseLeave(){
+    //console.log("아아 마우스는 떠났습니다.");
+    title.innerText="아아 마우스는 떠났습니다.";
+}
+//이벤트 실행
 title.addEventListener("click", handTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
