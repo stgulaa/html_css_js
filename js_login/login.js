e99cbd923b9da312ac1ff98097222ccaa3cf2e23
stgulaa <stgulaa@naver.com>
2021-07-22 00:11:58 +0900

    .preventDefault()

--- js_login/login.js
+++ js_login/login.js
@@ -1,9 +1,27 @@
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const loginForm = document.querySelector("#login-form");
+//const loginButton = document.querySelector("#login-form button");
 
-function btnclick(){
+/*function btnclick(){
     //console.dir(loginInput);
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    if(username===""){
+        alert("이름을 적어주세요");
+    }
+    //else if(username.length < 9){
+        //alert("글자수는 8글자 이하으로 해주세요");
+    //}//html에서 require maxlength를 맞춰주는 것이 더 좋음
+    else{
+        console.log(username);
+    }
 }
 
 loginButton.addEventListener("click", btnclick);
+*/
+
+function LoginSubmit(event){
+    event.preventDefault();
+    console.log(event);
+}
+
+loginForm.addEventListener("submit", LoginSubmit);
