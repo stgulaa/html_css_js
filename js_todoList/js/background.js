cd3ed8446790e698bf9326d390b51914ea9a342d
stgulaa <stgulaa@naver.com>
2021-08-03 23:01:19 +0900

    img 깨짐현상 복구
    js파일들을 따로 폴더로 묶음,
    bgimg.src에서 image가 아닌 img로 변경

--- js_todoList/js/background.js
+++ js_todoList/js/background.js
@@ -1,6 +1,6 @@
 const images = ["0.jpeg", "1.jpeg", "2.jpeg","3.jpeg"];
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 const bgimg = document.createElement("img");
-bgimg.src= `images\'${chosenImage}`;
 
+bgimg.src= `img/${chosenImage}`;
 document.body.appendChild(bgimg);
