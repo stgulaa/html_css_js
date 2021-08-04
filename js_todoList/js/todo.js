43d6912679fa140334a02f86fa17335ef8fb2f32
stgulaa <stgulaa@naver.com>
2021-08-04 20:44:06 +0900

    loading todo
    localStorage에 저장
    새로고침해도 목록이 남아있을 수 있게 함

--- js_todoList/js/todo.js
+++ js_todoList/js/todo.js
@@ -2,10 +2,20 @@ const TodoForm = document.getElementById("todo-form");
 const TodoList = document.getElementById("todo_list");
 const toDoInput = TodoForm.querySelector("input");
 
+const TODO_KEY = "todos";
+let toDos =[]; //할 일 목록
+
+
+function saveTodos(){
+    localStorage.setItem(TODO_KEY, JSON.stringify(toDos)); //DB저장
+}
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value="";
+    toDos.push(newTodo); //배열에 저장
+    saveTodos(newTodo);
     paintTodo(newTodo);
 }
 
@@ -13,7 +23,6 @@ function btnClick(event){
     const li = event.target.parentElement;
     li.remove();
 }
-
 
 function paintTodo(newTodo){
     const li = document.createElement("li");
@@ -28,3 +37,10 @@ function paintTodo(newTodo){
     TodoList.appendChild(li);
 }
 TodoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedTodos = localStorage.getItem(TODO_KEY);
+if(savedTodos !== null){
+    const parsedTodos = JSON.parse(savedTodos);
+    toDos = parsedTodos;
+    parsedTodos.forEach(paintTodo);
+}
