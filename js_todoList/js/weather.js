7f815a9d2760c39e8064f3369cb9f72823882d2d
stgulaa <stgulaa@naver.com>
2021-08-05 21:59:05 +0900

    weather
    api이용

---
+++ js_todoList/js/weather.js
@@ -0,0 +1,19 @@
+const API_KEY = "d9132a76987985835560b865fb5de45b";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("해당 위치의 날씨를 찾을 수 없습니다.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
