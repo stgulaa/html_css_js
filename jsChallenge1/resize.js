923180f8d3fe24699f8038d70ce38d8389c6a854
stgulaa <stgulaa@naver.com>
2021-08-26 18:10:35 +0900

    Update resize.js
    색상 배열을 이용하는 조건과 모든 함수 핸들러는 superEventHandler 내부에 작성하라는 조건을 덧붙임

--- jsChallenge1/resize.js
+++ jsChallenge1/resize.js
@@ -1,22 +1,27 @@
 const text = document.querySelector("h2")
-function formResize(){
-    text.innerText = "you just resized!";
-    text.style.color ="yellow";
-}
-function clickText(){
-    text.innerText = "that was a right click!";
-    text.style.color ="pink";
-}
-function textin(){
-    text.innerText="the mouse is here!";
-    text.style.color ="blue";
-}
-function textout(){
-    text.innerText="The mouse is gone!";
-    text.style.color ="red";
-}
 
-text.addEventListener("contextmenu", clickText);
-text.addEventListener("mouseleave", textout);
-text.addEventListener("mouseenter", textin);
-window.addEventListener("resize", formResize);
+const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+
+const superEventHandler = {
+    formResize: function () {
+      text.innerText = "you just resized!";
+      text.style.color = colors[0];
+    },
+    clickText: function () {
+      text.innerText = "that was a right click!";
+      text.style.color = colors[1];
+    },
+    textin: function () {
+      text.innerText = "the mouse is here!";
+      text.style.color = colors[2];
+    },
+    textout: function () {
+      text.innerText = "The mouse is gone!";
+      text.style.color = colors[3];
+    }
+  };
+
+  text.addEventListener("contextmenu", superEventHandler.clickText);
+  text.addEventListener("mouseleave", superEventHandler.textout);
+  text.addEventListener("mouseenter", superEventHandler.textin);
+  window.addEventListener("resize", superEventHandler.formResize);
