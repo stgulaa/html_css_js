724efb0538a1a4d8e6aa5e348229fee5eb15434a
stgulaa <stgulaa@naver.com>
2021-08-31 19:39:28 +0900

    Update RandomNumberGame.js
    랜덤 값 게임 구현 완료

--- jsChallenge3/RandomNumberGame.js
+++ jsChallenge3/RandomNumberGame.js
@@ -1,20 +1,27 @@
-const numberRange = document.querySelector("input #rangeNumber");
-const choosedNumer = document.querySelector("input #number");
-const play = document.querySelector("input #play");
+const form = document.querySelector("form");
+const numberRange = document.querySelector("#rangeNumber");
+const choosedNumer = document.querySelector("#number");
+const progress = document.querySelector("#progress");
+const outcome = document.querySelector("#outcome");
 
-function clickBtn(){
-    preventDefault();
-    //numberRange 범위값, chooseNumber 값 가져오기
-    //비교하기
+function clickBtn(event){
+    event.preventDefault();
+    const setNumberRange = numberRange.value;
+    const setChoosedNumber = choosedNumer.value;
+    const randomNum = Math.ceil(Math.random()*setNumberRange);
+    /*console.log(randomNum);
+    console.log(setChoosedNumber);
+    console.log(setNumberRange);*/
+    //비교하여 승패
+    if(randomNum==setChoosedNumber){
+        outcome.innerText = "you Win!"
+    }
+    else{
+        outcome.innerText = "you Lose!"
+    }
     //비교한 값 출력
-    //승패 알리기
+    progress.innerText="You chose : " + setChoosedNumber+", the machine chose "+randomNum;
+    
 }
 
-play.addEventListener("click", clickBtn);
-
-/*
-0에서 사용자가 지정한 숫자까지의 범위에서 랜덤 한 숫자를 찾으세요.(범위는 0 이상 입력값 이하가 됩니다.)
-범위에는 음수가 포함될 수 없습니다.
-실시간으로 범위 값을 업데이트해야 합니다.
-유저가 숫자를 선택한 후에 게임을 플레이할 수 있습니다.
-유저에게 게임의 승패를 알려야 합니다.*/
+form.addEventListener("submit", clickBtn);
