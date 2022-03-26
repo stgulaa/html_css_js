12734b46553d6bb8e98bed716beaec02c40d420d
stgulaa <stgulaa@naver.com>
2022-03-26 23:26:29 +0900

    Update userFunction.js
    rewrite for posting blog(feat.card capture sakura)

--- userFunction/userFunction.js
+++ userFunction/userFunction.js
@@ -9,3 +9,15 @@ function User(name, age){
 let user1 = new User('mike', 30);
 let user2 = new User('tom',15);
 user1.sayName();
+
+
+function Wizard(name, card, wand){
+    this.name=name;
+    this.card=card;
+    this.wand=wand;
+    this.magic= function(){ //메서드
+        console.log("뭔가 대단해 엄청나 마법");
+    }
+}
+const magicGirl = new Wizard('체리','크로우카드', '닭봉');
+magicGirl.magic();
