3d82ca0902d183bc20286590369d3d70e7662761
stgulaa <stgulaa@naver.com>
2022-03-30 16:38:43 +0900

    box move2

--- moving_box/moving.js
+++ moving_box/moving.js
@@ -1,9 +1,26 @@
-const box = document.getElementById("box");
-box.addEventListener('mousedown',(event)=>{
-    //박스 움직이기
-    let x = event.clientX;
-    let y = event.clientY;
-    box.style.position(x,y);
+const box = document.querySelector("#box");
+let isDragging=false;
+let original_x=null;
+let original_y=null;
+
+box.addEventListener("mouseup",(event)=>{
+    //마우스  올릴 때
+    isDragging=true; 
+    //원래 위치
+    original_x = event.clientX;
+    original_y = event.clientY;
 });
 
-box.addEventListener('');
+box.addEventListener("mousedown", (event) => {
+    //마우스 내릴 때
+    isDragging=false;
+    //현재 위치
+});
+box.addEventListener("mousemove", (event) => {
+    //마우스 움직일 때
+    if(isDragging==true){
+        //현재 위치
+        box.style.left = event.clientX +original_x;
+        box.style.top = event.clientY+original_y;
+    }
+});
